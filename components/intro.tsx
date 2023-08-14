6b3677a97b5850db583d01a945a0b81be200592a
kind012 <tranthienduc1001@gmal.com>
2023-08-14 14:12:40 +0700

    add logic intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { BsArrowRight, BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import Image from "next/image";
-import ManImage from "@/public/man.jpg";
+import { useSectionInView } from "@/hooks/useSectionInView";
 import Link from "next/link";
+import { FaPaperPlane } from "react-icons/fa";
 
 const Intro = () => {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -19,33 +25,35 @@ const Intro = () => {
               type: "tween",
               duration: 0.2,
             }}
-          >
-            <Image
-              src={ManImage}
-              alt="A man"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
-            />
-          </motion.div>
+          ></motion.div>
 
-          <span className="text-4xl absolute bottom-0 right-0">👋</span>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+            className="text-4xl absolute bottom-0 right-0"
+          >
+            👋
+          </motion.span>
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px4 text-2xl font-medium leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Duc.</span> I'm a{" "}
-        <span className="font-bold">front-end developer</span> with{" "}
-        <span className="font-bold">love</span> of design web. I enjoy building{" "}
+        <span className="font-bold">front-end developer</span> and{" "}
+        <span className="font-bold">love</span> of design. I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -56,15 +64,15 @@ const Intro = () => {
         }}
       >
         <Link
-          href="#contact"
+          href="https://www.facebook.com/tranthienduc1001/"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
           Contact me here{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer boderBlack dark:bg-white/10 dark:text-white/60"
           href="/CV.pdf"
           download
         >
@@ -73,7 +81,7 @@ const Intro = () => {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer boderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/"
           target="_blank"
         >
