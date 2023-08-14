7f013c192097c3ec3670cc6ee2f5990581151bad
kind012 <tranthienduc1001@gmal.com>
2023-08-14 14:10:16 +0700

    add logic footer

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <p className=" text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App Router), TypeScript, Tailwind CSS, Framer Motion,
+        Vercel hosting.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
